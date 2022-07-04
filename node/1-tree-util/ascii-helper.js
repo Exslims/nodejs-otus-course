48fde1119e98a01d843fc86cf71d8e474f3c0b48
konstantinkupcov <exslims1@mail.ru>
2022-07-04 23:34:34 +0300

    remove unused code

--- node/1-tree-util/ascii-helper.js
+++ node/1-tree-util/ascii-helper.js
@@ -1,27 +1,3 @@
-const treeObj = {
-    'name': 1, 'items': [{
-        'name': 2, 'items': [{
-            'name': 3
-        }, {
-            'name': 4, 'items': [{
-                'name': 7
-            }]
-        }, {
-            'name': 3
-        }]
-    }, {
-        'name': 5, 'items': [{
-            'name': 7, 'items': [{
-                'name': 3
-            }]
-        }, {
-            'name': 7, 'items': [{
-                'name': 3
-            }]
-        }]
-    }]
-};
-
 const buildAsciiTree = (child, indent, last) => {
     let result = indent;
 
@@ -50,46 +26,3 @@ const getPrettyAsciiTree = (obj) => {
 module.exports = {
     getPrettyAsciiTree
 }
-
-/*
-// not working
-const buildObjectTreeString = (elements, deepLvl, parentNotLast) => {
-    let result = '';
-
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        const last = i === elements.length - 1;
-        result += (parentNotLast ? `|   ` : '    ').repeat(deepLvl);
-        result += (!last ? `├── ${element.name}` : `└── ${element.name}`) + '\n';
-        if (element.items && element.items.length) {
-            result += buildObjectTreeString(element.items, deepLvl + 1, !last);
-        }
-    }
-    return result;
-};
-*/
-
-
-/*
-0 - └── {4}
-1 - |   └── {4}
-2 - |   |    └──{4}
-├ - если первый
-└ - если последний
-│ - в ином случае
- */
-
-/*
-└── 1
-    ├── 2
-    |   ├── 3
-    |   ├── 4
-    |   |   └── 7
-    |   └── 3
-    └── 5
-        ├── 7
-        |   └── 3
-        └── 7
-            └── 3
- */
-
